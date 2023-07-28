434c1caa1d53d085b03dfd8c52d7f8b9a3f630a8
go2Cshop <shell800808@gmail.com>
2023-07-28 15:33:02 +0800

    feat:add message partials

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const flash = require('connect-flash')// 引用提示套件
+const flash = require('connect-flash')
 
 const routes =require('./routes')
 
@@ -26,12 +26,12 @@ app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
-app.use(flash()) // 掛載套件
-app.use((req, res, next) => {
+app.use(flash())
+app.use((req,res,next)=>{
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
-  res.locals.success_msg = req.flash('success_msg') // 設定 success_msg 訊息
-  res.locals.warning_msg = req.flash('warning_msg') // 設定 warning_msg 訊息
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 

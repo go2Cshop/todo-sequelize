cb99e3d9019d491ae77966e53cb60a1bb39a3e44
go2Cshop <shell800808@gmail.com>
2023-07-28 14:47:11 +0800

    feat complete login & register function

--- app.js
+++ app.js
@@ -1,32 +1,104 @@
+// 引用 passport，放在文件上方
+const passport = require('passport')
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')//引用提示套件
+
 const bcrypt = require('bcryptjs')
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+
 const app = express()
 const PORT = 3000
+
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(express.urlencoded({ extendened: true }))
+
+app.use(session({
+  secret:'ThisIsMySecret',
+  resave: false,
+  saveUninitialized:true
+}))
+
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
+app.use(flash())  // 掛載套件
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => {
+      // console.log(todos)
+      return res.render('index', { todos: todos })
+    })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
 })
 
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local',{
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
 })
 
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
 })
 
 app.get('/users/logout', (req, res) => {

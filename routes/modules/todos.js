bdd9c3bb97c67db8420ddb747a36d69f2e928827
go2Cshop <shell800808@gmail.com>
2023-07-28 16:56:21 +0800

    feat:add delete todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -38,19 +38,29 @@ router.get('/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.put('/:id',(req,res)=>{
+router.put('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
-  const {name, isDone} = req.body
+  const { name, isDone } = req.body
   console.log(req.body)
 
-  return Todo.findOne({where: { id, UserId }})
+  return Todo.findOne({ where: { id, UserId } })
     .then(todo => {
-      todo.name=name
-      todo.isDone = isDone ==='on'
+      todo.name = name
+      todo.isDone = isDone === 'on'
       return todo.save()
     })
-    .then(()=>res.redirect(`/todos/${id}`))
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => todo.destroy())
+    //To delete rows of data from your SQL table using Sequelize, you need to use the provided destroy() method.
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 

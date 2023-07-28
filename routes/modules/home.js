ba30e1a534bcb7721309c187d9df4ad39ed1990f
go2Cshop <shell800808@gmail.com>
2023-07-28 15:45:15 +0800

    feat:add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,18 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 const db = require('../../models')
-const Todo =db.Todo
+const { where } = require('sequelize')
+const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => {
-      // console.log(todos)
-      return res.render('index', { todos })
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error('user not found')
+
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId: req.user.id }
+      })
     })
+    .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
-})
+  })
 
 module.exports = router
